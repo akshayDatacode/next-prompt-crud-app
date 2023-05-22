665455556945b7fc2e045ce7fdd408cd096c8234
akshayDatacode <akshay.webllisto@gmail.com>
2023-05-22 18:32:01 +0530

    handle auth error page

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -16,6 +16,9 @@ const handler = NextAuth({
     //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // })
   ],
+  pages: {
+    error: "/auth/error",
+  },
   secret: process.env.NEXTAUTH_URL_SECRET,
   callbacks: {
     async session({ session }) {
