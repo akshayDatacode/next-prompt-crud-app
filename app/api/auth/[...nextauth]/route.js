85cf9d63376ce2649d5f1594d830792903735e45
akshayDatacode <akshay.webllisto@gmail.com>
2023-05-23 16:24:08 +0530

    user existed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -34,13 +34,13 @@ const handler = NextAuth({
         const userExists = await User.findOne({ email: profile.email })
         console.log("userExists ==>", userExists)
         // if not, create a new user
-        // if (!userExists || userExists == null) {
+        if (!userExists || userExists === null) {
           await User.create({
             email: profile.email,
             userName: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture
           })
-        // }
+        }
         return true
       } catch (error) {
         console.log(error)
